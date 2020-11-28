f19d74297c5e6092c5e6dfa62e135ce4cc58378b
juanRodriGracia <juanico11@hotmail.com>
2020-11-28 17:12:07 +0100

    last day

--- WebContent/lib/js/mapa.js
+++ WebContent/lib/js/mapa.js
@@ -1,6 +1,6 @@
 const tilesProvider = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
-let myMap = L.map('mapid').setView([51.505, -0.09], 13);
+let myMap = L.map('mapid').setView([40.463667, -3.74922], 6);
 
 let grupoCirculos = L.layerGroup();
 
@@ -8,11 +8,66 @@ L.tileLayer(tilesProvider,{
 	maxZoom: 18,
 }).addTo(myMap);
 
+//marca de agua
+L.Control.Watermark = L.Control.extend({
+    onAdd: function(map) {
+        var img = L.DomUtil.create('img');
+
+        img.src = './img/gallery/covid.png';
+        img.style.width = '60px';
+
+        return img;
+    },
+
+    onRemove: function(map) {
+        // Nothing to do here
+    }
+});
+
+L.control.watermark = function(opts) {
+    return new L.Control.Watermark(opts);
+}
+
+L.control.watermark({ position: 'bottomleft' }).addTo(myMap);
+
+
+//leyenda mapa
+var legend = L.control({position: 'bottomright'});
+
+	legend.onAdd = function (map) {
+
+		var div = L.DomUtil.create('div', 'info legend'),
+			grades = [0, 20, 50, 70, 100],
+			labels = [],
+			from, to;
+
+		for (var i = 0; i < grades.length -1; i++) {
+			from = grades[i];
+			to = grades[i + 1];
+			if(from == 0){
+				labels.push(
+				'<i style="background:white;">Na</i> ' +
+				from + (to ? '&ndash;' + to : '+'));
+			}else{
+				labels.push(
+				'<i style="background:' + getColor(from + 1) + '"></i> ' +
+				from + (to ? '&ndash;' + to : '+'));
+			}
+			
+		}
+
+		div.innerHTML = labels.join('<br>');
+		return div;
+	};
+
+	legend.addTo(myMap);
+
+//localizacion de la posicion
 function onLocationFound(e) {
 		var radius = e.accuracy / 2;
 
 		L.marker(e.latlng).addTo(myMap)
-			.bindPopup("You are within " + radius + " meters from this point").openPopup();
+			.bindPopup("Tu estas entre  " + radius + " metros de este punto").openPopup();
 
 		L.circle(e.latlng, radius).addTo(myMap);
 	}
@@ -26,6 +81,8 @@ function onLocationFound(e) {
 
 	myMap.locate({setView: true, maxZoom: 16});
 
+
+//añadir y eliminar circulos mapa
 let bandera = false;
 function anadirCirculos(data) {
 	myMap.setView([data.latitud,data.longitud],8);
@@ -34,12 +91,29 @@ function anadirCirculos(data) {
 	}
 	let circulo;
 	for (var i = 0; i < data.regiones.length; i++) {
-		circulo = L.circle([data.regiones[i].latitud,data.regiones[i].longitud], 5000, {
-		color: 'red',
-		fillColor: '#f03',
-		fillOpacity: 0.5
-		});
-		grupoCirculos.addLayer(circulo);
+		var tamanioCirculo = calcularTamanio(data.regiones[i].habitantes);
+		if(data.regiones[i].factor > 20 && data.regiones[i].factor < 50){
+			circulo = L.circle([data.regiones[i].latitud,data.regiones[i].longitud], tamanioCirculo, {//5000
+			color: 'yellow',
+			fillColor: '#f7f85f',
+			fillOpacity: 0.5
+			});
+			grupoCirculos.addLayer(circulo);
+		}else if(data.regiones[i].factor >= 50 && data.regiones[i].factor < 70){
+			circulo = L.circle([data.regiones[i].latitud,data.regiones[i].longitud], tamanioCirculo, {
+			color: 'orange',
+			fillColor: '#ffa500',
+			fillOpacity: 0.5
+			});
+			grupoCirculos.addLayer(circulo);
+		}else if(data.regiones[i].factor >= 70){
+			circulo = L.circle([data.regiones[i].latitud,data.regiones[i].longitud], tamanioCirculo, {
+			color: 'red',
+			fillColor: '#f03',
+			fillOpacity: 0.5
+			});
+			grupoCirculos.addLayer(circulo);
+		}
 		//.addTo(myMap).bindPopup(data.regiones[i].nombre);
 	}
 	var i = 0;
@@ -50,5 +124,31 @@ function anadirCirculos(data) {
 	grupoCirculos.addTo(myMap);
 	bandera = true;
 	
-}	
+}
+
+// color de la leyenda
+	function getColor(d) {
+
+		if(d>70){
+			return '#f03';
+		}else if(d >= 50){
+			return '#ffa500';
+		}else if(d >= 20){
+			return '#f7f85f';
+		}
+		return 'green';
+	}
+
+//tamaño circulos diferentes sitios
+function calcularTamanio(tamanio){
+
+	if(tamanio<1500){
+		return tamanio;
+	}else{
+		return 6000;
+	}
+	
+	//(data.regiones[i].habitantes/100)
+
+}
 
